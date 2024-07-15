751e221f4dbef00b607e50b7efdfaa1d4602fcb5
Kenneth Hanson <khanson679@gmail.com>
2024-07-15 14:07:54 -0400

    Make printable. Don't treat terminals like rules.

--- treegrammar.py
+++ treegrammar.py
@@ -8,14 +8,16 @@ class TreeGrammar:
         Contruct grammar from list of tuples of form (parent, children).
         """
         self.start = None
+
         self.rules = []
         if rules:
             for rule in rules:
                 parent, children = rule
                 rules.append((parent, tuple(children)))
-        if terminals:
-            self.add_terminals(terminals)
-        self.any_terminal = False
+
+        if not terminals:
+            terminals = []
+        self.terminals = terminals
 
     def set_start(self, start):
         self.start = start
@@ -25,8 +27,19 @@ class TreeGrammar:
         self.rules.append((parent, children))
 
     def add_terminals(self, terminals):
-        for symb in terminals:
-            self.add_rule(symb, [])
+        self.terminals.extend(terminals)
+
+    def __str__(self):
+        rulestrings = []
+        for rule in self.rules:
+            parent = rule[0]
+            children = rule[1]
+            rulestrings.append(f"  {parent} -> {' '.join(children)}")
+        return (
+            f"Start: {self.start}" +
+            f"\nRules:\n" + '\n'.join(rulestrings) +
+            f"\nTerminals: {self.terminals}"
+        )
 
     # def set_any_terminal(self, flag=True):
     #     self.any_terminal = flag
@@ -39,19 +52,28 @@ class TreeGrammar:
             return False
         for node in tree.traverse():
             match = False
-            parent_label = node.label
-            child_labels = tuple(c.label for c in node.children)
-            for rule in self.rules:
-                # if self.any_terminal and not child_labels:
-                #     match = True
-                #     break
-                if self._match_subtree(rule, parent_label, child_labels):
-                    match = True
-                    break
+            if node.children:
+                # non-terminal node
+                parent_label = node.label
+                child_labels = tuple(c.label for c in node.children)
+                for rule in self.rules:
+                    # if self.any_terminal and not child_labels:
+                    #     match = True
+                    #     break
+                    if self._match_subtree(rule, parent_label, child_labels):
+                        match = True
+                        break
+            else:
+                # terminal node
+                for term in self.terminals:
+                    if re.match(term, node.label):
+                        match = True
+                        break
             if not match:
                 if debug:
-                    print(f"bad subtree: ({parent_label}, {child_labels})")
+                    print(f"bad subtree: ({parent_label}, [{child_labels}])")
                 return False
+
         return True
 
     @staticmethod
@@ -81,10 +103,13 @@ if __name__ == "__main__":
     g1.add_rule('V-tr', ['chased'])
     g1.add_terminals(['the', 'cat', 'rat', 'sat', 'chased'])
     # g1.set_any_terminal(True)
+    print(g1)
+    print()
 
     t1 = Tree.from_string("(S (NP (D the) (N cat)) (VP (V-tr chased) (NP (D the) (N rat))))")
     t2 = Tree.from_string("(S (NP (N cat) (D the)) (VP (NP (N rat) (D the)) (V-tr chased)))")
     t3 = Tree.from_string("(NP (D the) (N cat))")
+
     print(g1.recognize(t1, debug=True))
     print(g1.recognize(t2, debug=True))
     print(g1.recognize(t3, debug=True))
